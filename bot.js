35a09e1382906382a019224fb10237275f33a8a9
Brad <bradz.1708@gmail.com>
2018-04-15 10:59:50 +1000

    Attempting to fix bot downtime

--- bot.js
+++ bot.js
@@ -69,24 +69,27 @@ const init = async () => {
 
 try {
     init();
-    const express = require('express');
-    const app = express();
-    const moment = require('moment');
-    const chalk = require('chalk');
-    const timestamp = `[${moment().format('YYYY-MM-DD HH:mm:ss')}]`;
-
-    app.get('/', (request, response) => {
-      console.log(`${timestamp}: ${chalk.green('PING')}`);
-      response.sendStatus(200);
-    });
-
-    app.listen(process.env.PORT);
-
-    setInterval(() => {
-      http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-    }, 280000);
 } catch (e) {
     client.logger.error(e.stack);
     client.logger.error('Shutting down...');
     process.exit(1);
 }
+
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const moment = require('moment');
+const chalk = require('chalk');
+const timestamp = `[${moment().format('YYYY-MM-DD HH:mm:ss')}]`;
+
+app.get('/', (request, response) => {
+    console.log(`${timestamp}: ${chalk.green('PING')}`);
+    response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
