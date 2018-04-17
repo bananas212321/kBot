141b24d79af27a1e594d12eaf4acbfe0f5a3c857
Brad <bradz.1708@gmail.com>
2018-04-17 12:49:30 +1000

    Fixed alias and minor changes

--- commands/ping.js
+++ commands/ping.js
@@ -1,19 +1,19 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
     const msg = await message.channel.send('Pinging...');
-    msg.edit(`Pong! Latency is **${msg.createdTimestamp - message.createdTimestamp}ms**
-API Latency is **${Math.round(client.ping)}ms**`);
+    msg.edit(`Pong! Latency is **~${msg.createdTimestamp - message.createdTimestamp}ms**
+API Latency is **~${Math.round(client.ping)}ms**`);
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['ping'],
+    aliases: ['pong'],
     permLevel: 'User'
 };
 
 exports.help = {
     name: 'ping',
-    category: 'Miscelaneous',
+    category: 'Bot',
     description: 'It... like... pings. Then Pongs. And it\'s not Ping Pong.',
     usage: 'ping'
 };
