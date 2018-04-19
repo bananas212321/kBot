94abdc025a590a7d18e29521b624029a8382749c
Brad <bradz.1708@gmail.com>
2018-04-19 22:25:02 +1000

    Rady.js changes

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,13 @@
 module.exports = client => {
 	client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, 'ready');
 	console.log('');
-	client.user.setActivity(`${client.config.defaultSettings.prefix}status`, {
-		type: 'LISTENING'
-	});
+	const playingMsg = `${client.config.defaultSettings.prefix}status`;
+	try {
+		client.user.setActivity(playingMsg, {
+			type: 'LISTENING'
+		});
+		client.logger.debug(`Set playing message to ${playingMsg}`);
+	} catch (e) {
+		client.logger.error(e);
+	}
 };
