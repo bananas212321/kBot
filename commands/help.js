84de8a70d7dd05078d630c5126310fd7691f8130
Brad <bradz.1708@gmail.com>
2018-04-28 16:12:17 +1000

    Kind of overhauled the help command to categorise it

--- commands/help.js
+++ commands/help.js
@@ -2,12 +2,27 @@ const { RichEmbed } = require('discord.js');
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
     let embed = new RichEmbed()
-        .setAuthor('kBot Commands List', client.user.avatarURL)
-        .setColor(0xffffff);
-    client.commands.array().forEach(cmd => {
-        embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)}`, cmd.help.description)
-    });
-    message.channel.send(embed);
+        .setAuthor('kBot Commands List', client.user.avatarURL).setColor(0xffffff);
+        embed.addField('Category: Guild Settings', '-');
+        await client.commands.array().forEach(cmd => {
+            if(cmd.help.category == 'Guild') {
+                embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)} (Guild only: ${cmd.conf.guildOnly})`, cmd.help.description);
+            };
+        });
+        embed.addBlankField().addField('Category: Moderation', '-');
+        await client.commands.array().forEach(cmd => {
+            if(cmd.help.category == 'Moderation') {
+                embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)} (Guild only: ${cmd.conf.guildOnly})`, cmd.help.description);
+            };
+        });
+        embed.addBlankField().addField('Category: Bot', '-');
+        await client.commands.array().forEach(cmd => {
+            if(cmd.help.category == 'Bot') {
+                embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)} (Guild only: ${cmd.conf.guildOnly})`, cmd.help.description);
+            };
+        });
+        embed.setTimestamp();
+    return message.channel.send(embed);
 };
 
 exports.conf = {
