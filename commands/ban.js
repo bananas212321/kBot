f44e0ac10a5e730a0478e7eb04274461062fc91f
Brad <bradz.1708@gmail.com>
2018-04-28 22:49:35 +1000

    Revert some ban.js changes

--- commands/ban.js
+++ commands/ban.js
@@ -3,84 +3,46 @@ const { RichEmbed } = require('discord.js');
 exports.run = async (client, msg, args, level) => { // eslint-disable-line no-unused-vars
     try {
         /* Check to make sure that there is a user to kick */
-        if(!args[0]) return msg.reply(':warning: No user to kick was provided!');
-        if(!msg.mentions.users.first()) return msg.reply(':warning: No user to kick was provided!');
+        if(!args[0]) return msg.reply(':warning: No user to ban was provided!');
+        if(!msg.mentions.users.first()) return msg.reply(':warning: No user to ban was provided!');
 
         /* Make sure that you can kick the user */
-        let user = await msg.guild.member(msg.mentions.users.first() || args[0]);
-        if(!user) msg.reply(':warning: I\'m unable to find that user.');
-        if(!user.kickable) msg.reply(':warning: I\'m unable to kick that user.');
-
-        let days = parseInt(args[1]);
-        if(days > 7 || days < 0) return msg.reply(':warning: You can not delete more than 7 days worth of messages.');
-
+        let user = await msg.guild.member(msg.mentions.users.first());
+        if(!user) return msg.reply(':warning: I\'m unable to find that user.');
+        if(!user.bannable) return msg.reply(':warning: I\'m unable to ban that user.');
         /* Kick the user */
-        if(!days) {
-            if(!args[1]) {
-                try {
-                    let reason = 'No reason given.';
-                    await msg.guild.ban(user, { reason: reason });
-                    let embed = new RichEmbed()
-                        .setAuthor(`Banned ${user.user.tag}`, user.avatarURL)
-                        .addField('Reason', reason)
-                        .setTimestamp()
-                        .setColor(0xffffff);
-                    return msg.channel.send(embed);
-                } catch (e) {
-                    await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
-                    return client.logger.error(e.stack);
-                };
-            } else {
-                try {
-                    args.shift();
-                    let reason = args.join(' ');
-                    await msg.guild.ban(user, { reason: reason });
-                    let embed = new RichEmbed()
-                        .setAuthor(`Banned ${user.user.tag}`, user.user.avatarURL)
-                        .addField('Reason', reason)
-                        .setTimestamp()
-                        .setColor(0xffffff);
-                    return msg.channel.send(embed);
-                } catch (e) {
-                    await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
-                    return client.logger.error(e.stack);
-                };
+        if(!args[1]) {
+            try {                    let reason = 'No reason given.';
+                await msg.guild.ban(user, { reason: reason });
+                let embed = new RichEmbed()
+                    .setAuthor(`Banned ${user.user.tag}`, user.avatarURL)
+                    .addField('Reason', reason)
+                    .setTimestamp()
+                    .setColor(0xffffff);
+                return msg.channel.send(embed);
+            } catch (e) {
+                await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
+                return client.logger.error(e.stack);
             };
         } else {
-            if(!args[1]) {
-                try {
-                    let reason = 'No reason given.';
-                    await msg.guild.ban(user, { reason: reason, days: days });
-                    let embed = new RichEmbed()
-                        .setAuthor(`Banned ${user.user.tag}`, user.avatarURL)
-                        .addField('Reason', reason)
-                        .setTimestamp()
-                        .setColor(0xffffff);
-                    return msg.channel.send(embed);
-                } catch (e) {
-                    await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
-                    return client.logger.error(e.stack);
-                };
-            } else {
-                try {
-                    args.shift(); args.shift();
-                    let reason = args.join(' ');
-                    await msg.guild.ban(user, { reason: reason, days: days });
-                    let embed = new RichEmbed()
-                        .setAuthor(`Banned ${user.user.tag}`, user.user.avatarURL)
-                        .addField('Reason', reason)
-                        .setTimestamp()
-                        .setColor(0xffffff);
-                    return msg.channel.send(embed);
-                } catch (e) {
-                    await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
-                    return client.logger.error(e.stack);
-                };
-            };
+            try {
+                args.shift();
+                let reason = args.join(' ');
+                await msg.guild.ban(user, { reason: reason });
+                let embed = new RichEmbed()
+                    .setAuthor(`Banned ${user.user.tag}`, user.user.avatarURL)
+                    .addField('Reason', reason)
+                    .setTimestamp()
+                	.setColor(0xffffff);
+                return msg.channel.send(embed);
+			} catch (e) {
+				await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
+				return client.logger.error(e.stack);
+			};
         };
     } catch (e) {
         await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
-		return client.logger.error(e.stack);
+        return client.logger.error(e.stack);
     };
 };
 
