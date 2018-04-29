d26bf13410050fefa2c866f48e0524f60114407b
Brad <bradz.1708@gmail.com>
2018-04-29 22:51:48 +1000

    Improved error checking on urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -1,18 +1,23 @@
 const urban = require('urban');
 const { RichEmbed } = require('discord.js');
 
-exports.run = (client, msg, args, level) => { // eslint-disable-line no-unused-vars
-	if(!args[0]) msg.reply(':warning: You can\'t search for nothing!');
-	urban(args[0]).first((json) => {
-		if(!json) return msg.reply(`:warning: Nothing was found for **${args[0]}**.`);
-		let embed = new RichEmbed()
-			.setAuthor(`Urban Dictionary results for: ${args[0]}`, 'https://vignette.wikia.nocookie.net/logopedia/images/a/a7/UDAppIcon.jpg/revision/latest?cb=20170422211150')
-			.addField('Definition', json.definition.length <= 1024 ? json.definition : `Truncated due to exceeding maximum length\n${json.definition.slice(0, 970)}`)
-			.addField('Example', json.example.length <= 1024 ? json.example : `Truncated due to exceeding maximum length\n${json.example.slice(0, 970)}`)
-			.setFooter(`Permalink: ${json.permalink}`)
-			.setColor(0xffffff);
-		return msg.channel.send(embed);
-	});
+exports.run = async (client, msg, args, level) => { // eslint-disable-line no-unused-vars
+	try {
+		if(!args[0]) msg.reply(':warning: You can\'t search for nothing!');
+		urban(args[0]).first((json) => {
+			if(!json) return msg.reply(`:warning: Nothing was found for **${args[0]}**.`);
+			let embed = new RichEmbed()
+				.setAuthor(`Urban Dictionary results for: ${args[0]}`, 'https://vignette.wikia.nocookie.net/logopedia/images/a/a7/UDAppIcon.jpg/revision/latest?cb=20170422211150')
+				.addField('Definition', json.definition.length <= 1024 ? json.definition : `Truncated due to exceeding maximum length\n${json.definition.slice(0, 970)}`)
+				.addField('Example', json.example.length <= 1024 ? json.example : `Truncated due to exceeding maximum length\n${json.example.slice(0, 970)}`)
+				.setFooter(`Permalink: ${json.permalink}`)
+				.setColor(0xffffff);
+			return msg.channel.send(embed);
+		});	
+	} catch (e) {
+		await msg.reply(`:no_entry_sign: An unexpected error occurred!\n**Details:**\n\`\`\`diff\n- ${e.stack}\`\`\``);
+		return client.logger.error(e.stack);
+	}
 };
 
 exports.conf = {
