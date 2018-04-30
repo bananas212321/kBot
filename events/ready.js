b25c1c02e39c798c49253c7a694857b77fac4f69
Brad <bradz.1708@gmail.com>
2018-04-30 12:48:34 +1000

    Set new playing message and made ready.js async

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
-module.exports = client => {
+module.exports = async (client) => {
 	client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, 'ready');
 	console.log('');
-	const playingMsg = `${client.config.defaultSettings.prefix}status`;
+	const playingMsg = `${client.config.defaultSettings.prefix}help`;
 	try {
-		client.user.setActivity(playingMsg, {
+		await client.user.setActivity(playingMsg, {
 			type: 'LISTENING'
 		});
-		client.logger.debug(`Set playing message to ${playingMsg}`);
+		return client.logger.debug(`Set playing message to ${playingMsg}`);
 	} catch (e) {
 		return client.logger.error(e);
 	}
