31c5a68b3b83912cf245fb368a5d10bd3d079bd8
Brad <bradz.1708@gmail.com>
2018-04-30 22:41:32 +1000

    Some fixes to Urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -3,11 +3,11 @@ const { RichEmbed } = require('discord.js');
 
 exports.run = async (client, msg, args, level) => { // eslint-disable-line no-unused-vars
 	try {
-		if(!args[0]) msg.reply(':warning: You can\'t search for nothing!');
-		urban(args[0]).first((json) => {
-			if(!json) return msg.reply(`:warning: Nothing was found for **${args[0]}**.`);
+		if(!args.join(' ')) return msg.reply(':warning: You can\'t search for nothing!');
+		urban(args.join(' ')).first((json) => {
+			if(!json) return msg.reply(`:warning: Nothing was found for **${args.join(' ')}**.`);
 			let embed = new RichEmbed()
-				.setAuthor(`Urban Dictionary results for: ${args[0]}`, 'https://vignette.wikia.nocookie.net/logopedia/images/a/a7/UDAppIcon.jpg/revision/latest?cb=20170422211150')
+				.setAuthor(`Urban Dictionary results for: ${args.join(' ')}`, 'https://vignette.wikia.nocookie.net/logopedia/images/a/a7/UDAppIcon.jpg/revision/latest?cb=20170422211150')
 				.addField('Definition', json.definition.length <= 1024 ? json.definition : `Truncated due to exceeding maximum length\n${json.definition.slice(0, 970)}`)
 				.addField('Example', json.example.length <= 1024 ? json.example : `Truncated due to exceeding maximum length\n${json.example.slice(0, 970)}`)
 				.setFooter(`Permalink: ${json.permalink}`)
