18bd40ac13ae4a761ef0f7115f9fe33bce481615
Brad <bradz.1708@gmail.com>
2018-05-03 00:16:25 +1000

    Debug.js now shows process memory usage and uptime

--- events/debug.js
+++ events/debug.js
@@ -1,4 +1,12 @@
 module.exports = (client, debug) => { /* eslint-disable no-console */
 	if(debug.includes(client.config.token)) return;
-	return client.logger.log(`${debug}`, 'debug');
+	if(debug.startsWith('[ws] [connection] Heartbeat acknowledged, ')) {
+		client.logger.debug(debug);
+		const used = process.memoryUsage();
+		for (let key in used) {
+			client.logger.debug(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+		}
+	} else {
+		return client.logger.debug(debug);
+	}
 };
