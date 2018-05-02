6ddb1f853af189ca20f9d0ea1a46d6a7fa998241
Brad <bradz.1708@gmail.com>
2018-05-03 00:16:35 +1000

    Debug

--- events/debug.js
+++ events/debug.js
@@ -4,9 +4,11 @@ module.exports = (client, debug) => { /* eslint-disable no-console */
 		client.logger.debug(debug);
 		const used = process.memoryUsage();
 		for (let key in used) {
-			client.logger.debug(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
-		}
+			client.logger.debug(` - ${key.toUpperCase()}: ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+		};
+		client.logger.debug(`Uptime: ${Math.floor(process.uptime() / 600)}:${Math.floor(process.uptime() / 60)}:${Math.floor(process.uptime())}`);
+		return console.log('');
 	} else {
-		return client.logger.debug(debug);
+		client.logger.debug(debug);
 	}
 };
