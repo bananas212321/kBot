a80aa75caf1efb673189c30c3279627520388803
Brad <bradz.1708@gmail.com>
2018-05-21 23:44:15 +1000

    Removed some code from ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,6 @@ module.exports = async (client) => {
   	console.log('\u200b');
 	client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, 'ready');
 	console.log('\u200b');
-	let guilds = client.guilds.array();
-	guilds.forEach(guild => {
-		client.logger.debug(`Name: ${guild.name}, ID: ${guild.id}, Avalible?: ${guild.available}`);
-	});
-	console.log('\u200b');
 	try {
 		client.user.setActivity(`k!help | ${client.users.size} users in ${client.channels.size} channels for ${client.guilds.size} guilds`, {
 			type: 'LISTENING'
