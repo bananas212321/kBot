875d6120982b396d633e628922b68b60f8441c97
Brad <bradz.1708@gmail.com>
2018-05-26 00:03:26 +1000

    Overhauled the help command, with some external help, see L1

--- commands/help.js
+++ commands/help.js
@@ -1,52 +1,52 @@
-const { RichEmbed } = require('discord.js');
+/* Credit https://github.com/AnIdiotsGuide/guidebot/blob/e2e717d2a09db80aa03bc47bf07cd5361f50a622/commands/help.js */
 
-exports.run = async (client, msg, args, level) => { // eslint-disable-line no-unused-vars
-    try {
-        let embed = new RichEmbed()
-            .setAuthor('kBot Commands List', client.user.avatarURL).setColor(0xffffff);
-            embed.addField('Category: Guild Settings', '-');
-            await client.commands.array().forEach(cmd => {
-                if(cmd.help.category == 'Guild') {
-                    embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)} (Guild only: ${cmd.conf.guildOnly})`, cmd.help.description);
-                };
-            });
-            embed.addBlankField().addField('Category: Moderation', '-');
-            await client.commands.array().forEach(cmd => {
-                if(cmd.help.category == 'Moderation') {
-                    embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)} (Guild only: ${cmd.conf.guildOnly})`, cmd.help.description);
-                };
-            });
-            embed.addBlankField().addField('Category: Bot', '-');
-            await client.commands.array().forEach(cmd => {
-                if(cmd.help.category == 'Bot') {
-                    embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)} (Guild only: ${cmd.conf.guildOnly})`, cmd.help.description);
-                };
-            });
-            embed.addBlankField().addField('Category: Fun', '-');
-            await client.commands.array().forEach(cmd => {
-                if(cmd.help.category == 'Fun') {
-                    embed.addField(`${cmd.help.name.charAt(0).toUpperCase()}${cmd.help.name.substring(1)} (Guild only: ${cmd.conf.guildOnly})`, cmd.help.description);
-                };
-            });
-            embed.setTimestamp();
-        await msg.author.send(embed);
-        return msg.reply(':white_check_mark: I have just messaged you a list of commands, check your DMs!');
-    } catch (e) {
-        await msg.reply(':no_entry_sign: Oops! Something went wrong and an unexpected error occurred!');
-		return client.logger.error(e.stack);
-    };
+exports.run = async (client, message, args, level) => {
+	// If no specific command is called, show all filtered commands.
+	if (!args[0]) {
+		// Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
+		const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
+	
+		// Here we have to get the command names only, and we use that array to get the longest name.
+		// This make the help commands 'aligned' in the output.
+		const commandNames = myCommands.keyArray();
+		const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+	
+		let currentCategory = '';
+		let output = `= Command List =\n\n[Use ${message.settings.prefix}help <commandname> for details]\n`;
+		const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
+		sorted.forEach(c => {
+			const cat = c.help.category.toProperCase();
+			if (currentCategory !== cat) {
+				output += `\u200b\n== ${cat} ==\n`;
+				currentCategory = cat;
+			}
+			output += `${message.settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+		});
+		await message.reply(':white_check_mark: I have just messaged you a list of commands, check your DMs!')
+		return message.author.send(output, {code: 'asciidoc', split: { char: '\u200b' }});
+	} else {
+		// Show individual command's help.
+		let command = args[0];
+		if (client.commands.has(command)) {
+			command = client.commands.get(command);
+			if (level < client.levelCache[command.conf.permLevel]) return;
+			let prefix = 'k!'
+			if(message.guild && client.settings.has(message.guild.id)) prefix = message.settings.prefix; 
+			message.channel.send(`= ${command.help.name.toProperCase()} = \nDescription :: ${command.help.description}\nUsage :: ${prefix}${command.help.usage}\nAliases :: ${command.conf.aliases.join(', ')}`, { code: 'asciidoc'} );
+		}
+	}
 };
-
+  
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['cmds', 'commands'],
-    permLevel: 'User'
+	enabled: true,
+	guildOnly: false,
+	aliases: ['cmds', 'commands'],
+	permLevel: 'User'
 };
-
+  
 exports.help = {
-    name: 'help',
-    category: 'Bot',
-    description: 'Grab a list of kBot\'s commands, all in one tidy RichEmbed.',
-    usage: 'help'
+	name: 'help',
+	category: 'Bot',
+	description: 'Displays all the available commands for your permission level.',
+	usage: 'help [command]'
 };
